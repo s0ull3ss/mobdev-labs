90fc62bb7470ba4ed7bfa17599d55b179061d4b6
Soulless <14828321+s0ull3ss@users.noreply.github.com>
2018-02-20 23:36:53 +0200

    complete sample lab4

---
+++ lab4/lab4/MainActivity.cs
@@ -0,0 +1,59 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Hardware;
+using Android.Media;
+using Android.Runtime;
+
+namespace lab4
+{
+    [Activity(Label = "lab4", MainLauncher = true)]
+    public class MainActivity : Activity, ISensorEventListener
+    {
+        SensorManager sensorManager;
+        Android.Media.MediaPlayer mp;
+        bool play = false;
+
+        public void OnAccuracyChanged(Sensor sensor, [GeneratedEnum] SensorStatus accuracy)
+        {
+            //throw new System.NotImplementedException();
+        }
+
+        public void OnSensorChanged(SensorEvent e)
+        {
+            if (!mp.IsPlaying)
+            {
+                mp.Start();
+            }
+            else
+            {
+                mp.Pause();
+            }
+            //throw new System.NotImplementedException();
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            sensorManager.RegisterListener(this, sensorManager.GetDefaultSensor(SensorType.StepDetector), SensorDelay.Ui);
+        }
+
+        protected override void OnPause()
+        {
+            base.OnPause();
+            sensorManager.UnregisterListener(this);
+        }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            sensorManager = (SensorManager)GetSystemService(SensorService);
+            mp = MediaPlayer.Create(this, Resource.Raw.test);
+        }
+    }
+}
+
