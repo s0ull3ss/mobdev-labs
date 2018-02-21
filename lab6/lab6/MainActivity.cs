3652686ba4ed1af061cca3a461e44b7db954a327
Soulless <14828321+s0ull3ss@users.noreply.github.com>
2018-02-21 20:18:59 +0200

    complete lab6

---
+++ lab6/lab6/MainActivity.cs
@@ -0,0 +1,34 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System;
+
+namespace lab6
+{
+    public static class Intermediate
+    {
+        public static string URLNews = "http://www.bbc.com/";
+    }
+    [Activity(Label = "lab6", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        Button news;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            news = FindViewById<Button>(Resource.Id.news);
+            news.Click += NewsClick;
+        }
+
+        private void NewsClick(object sender, EventArgs e)
+        {
+            StartActivity(typeof(WebViewPage));
+        }
+    }
+}
+
